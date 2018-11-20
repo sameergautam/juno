2e011b7ad3672205f6eba55fdf10daf657998e2d
Aneeta Sharma <get.aneeta@gmail.com>
2018-11-20 14:02:46 +0545

    Add validation error messaging for login.

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './../style/App.scss';
 import axios from 'axios';
+import * as toastr from "toastr";
+import './../../node_modules/toastr/toastr.scss'
 
 class Login extends React.Component<any, any> {
   constructor(props: any) {
@@ -23,7 +25,7 @@ class Login extends React.Component<any, any> {
       })
       .catch((error) => {
         NProgress.done();
-        console.log(error.message);
+        toastr.error(error.response.data);
       });
   }
 
@@ -32,8 +34,8 @@ class Login extends React.Component<any, any> {
       <div className="login">
         <form onSubmit={this.logUser}>
           <img src="cf-separated-logo.png" alt="CloudFactory" width="300" className="logo" /><br />
-          <input type="text" name="username" id="username" placeholder="Username" /><br />
-          <input type="password" name="password" id="password" placeholder="Password" /><br />
+          <input type="text" name="username" id="username" placeholder="Username" required /><br />
+          <input type="password" name="password" id="password" placeholder="Password" required /><br />
           <button>Login</button>
         </form>
       </div>
